31db731e888871c2b24f83edf8475f0a466df52a
Hans Bentlöv <hbentlov@Hanss-MacBook-Pro.local>
2014-11-11 23:21:00 +0100

    Add station ID to each destination and origin

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express     =   require('express');
 var cheerio     =   require('cheerio');
 var bodyParser  =   require('body-parser');
 var request     =   require('request');
-
+var moment      =   require('moment');
 request('http://hertzfreerider.se/unauth/list_transport_offer.aspx', function(error, response, html) {
 
   var $ = cheerio.load(html);
@@ -10,19 +10,27 @@ request('http://hertzfreerider.se/unauth/list_transport_offer.aspx', function(er
   $('tr.highlight').each(function(i, item) {
     var anchors = $(item).find('a');
     var additionalInfo = $(this).next().find('td span');
-    var startDate = additionalInfo.eq(0).text();
-    var endData = additionalInfo.eq(1).text();
+    var startDate = moment(additionalInfo.eq(0).text()).format();
+    var endData = moment(additionalInfo.eq(1).text()).format();
     var car = additionalInfo.eq(2).text();
 
+    var fromStationId = anchors.eq(0).attr('href').split('stationId=')[1];
+    var toStationId = anchors.eq(1).attr('href').split('stationId=')[1];
+
     parsed.push({
-      from: anchors.eq(0).text(),
-      to: anchors.eq(1).text(),
+      from: {
+        name: anchors.eq(0).text(),
+        id: fromStationId
+      },
+      to: {
+        name: anchors.eq(1).text(),
+        id: toStationId
+      },
       car: car,
       startData: startDate,
       endData: endData
     });
   });
-
 });
 
 
