cc702a4a1ea2a03828071b131862687527dc9c39
Hans Bentlöv <hbentlov@Hanss-MacBook-Pro.local>
2014-11-12 00:00:10 +0100

    Remove commented code

--- index.js
+++ index.js
@@ -49,39 +49,6 @@ request(baseUrl + 'list_transport_offer.aspx', function(error, response, html) {
     console.log('done');
     console.log(parsed[0])
   });
-  /*$('tr.highlight').each(function(i, item) {
-    var anchors = $(item).find('a');
-    var additionalInfo = $(this).next().find('td span');
-    var startDate = moment(additionalInfo.eq(0).text()).format();
-    var endData = moment(additionalInfo.eq(1).text()).format();
-    var car = additionalInfo.eq(2).text();
-
-    var fromStationId = anchors.eq(0).attr('href').split('stationId=')[1];
-    var toStationId = anchors.eq(1).attr('href').split('stationId=')[1];
-
-    // Fetch information about the oritin station
-    request(baseUrl + 'stationInfo.aspx?stationId=' + fromStationId, function(error, response, html) {
-      parsed.push({
-        from: {
-          name: anchors.eq(0).text(),
-          id: fromStationId
-        },
-        to: {
-          name: anchors.eq(1).text(),
-          id: toStationId
-        },
-        car: car,
-        startData: startDate,
-        endData: endData
-      });
-      console.log('now its fetched')
-    });
-    console.log('im done');
-
-  });
-
-  async.map(parsed)
-*/
 });
 
 
