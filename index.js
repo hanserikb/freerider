ced2324137440b7bbb11c6f7d6b3ea398c532d56
Hans Bentlöv <hbentlov@Hanss-MacBook-Pro.local>
2014-11-12 17:47:09 +0100

    Encapsulate scarping in a express route

--- index.js
+++ index.js
@@ -6,83 +6,86 @@ var moment      =   require('moment');
 var async       =   require('async');
 var baseUrl = 'http://hertzfreerider.se/unauth/';
 
-request(baseUrl + 'list_transport_offer.aspx', function(error, response, html) {
+var app = express();
 
-  var $ = cheerio.load(html);
-  var parsed = [];
-  var poo = ($('tr.highlight'));
-  async.eachSeries(poo.splice(0, 2), function(item, theCallback) {
-    var anchors = $(item).find('a');
-    var additionalInfo = $(item).next().find('td span');
-    var startDate = moment(additionalInfo.eq(0).text()).format();
-    var endData = moment(additionalInfo.eq(1).text()).format();
-    var car = additionalInfo.eq(2).text();
 
-    var fromStationId = anchors.eq(0).attr('href').split('stationId=')[1];
-    var toStationId = anchors.eq(1).attr('href').split('stationId=')[1];
+app.get('/', function(req, res) {
+  request(baseUrl + 'list_transport_offer.aspx', function(error, response, html) {
+    var $ = cheerio.load(html);
+    var parsed = [];
+    var poo = ($('tr.highlight'));
+    async.eachSeries(poo.splice(0, 2), function(item, theCallback) {
+      var anchors = $(item).find('a');
+      var additionalInfo = $(item).next().find('td span');
+      var startDate = moment(additionalInfo.eq(0).text()).format();
+      var endData = moment(additionalInfo.eq(1).text()).format();
+      var car = additionalInfo.eq(2).text();
 
-    async.parallel([function(callback) {
-      request(baseUrl + 'stationInfo.aspx?stationId=' + fromStationId, function(error, response, html) {
-        var $$ = cheerio.load(html);
+      var fromStationId = anchors.eq(0).attr('href').split('stationId=')[1];
+      var toStationId = anchors.eq(1).attr('href').split('stationId=')[1];
 
-        var scraped = $$('.displayDriverData').find('tr td');
-        var stationData = {
-          location: {
-            street: scraped.eq(0).find('span').eq(0).text(),
-            postalCode: scraped.eq(0).find('span').eq(1).text(),
-            city: scraped.eq(0).find('span').eq(2).text()
+      async.parallel([function(callback) {
+        request(baseUrl + 'stationInfo.aspx?stationId=' + fromStationId, function(error, response, html) {
+          var $$ = cheerio.load(html);
+
+          var scraped = $$('.displayDriverData').find('tr td');
+          var stationData = {
+            location: {
+              street: scraped.eq(0).find('span').eq(0).text(),
+              postalCode: scraped.eq(0).find('span').eq(1).text(),
+              city: scraped.eq(0).find('span').eq(2).text()
+            },
+            contact: {
+              email: scraped.eq(1).find('span').eq(1).text(),
+              phone: scraped.eq(1).find('span').eq(2).text()
+            }
+          };
+
+          callback(null, stationData);
+        })
+      }, function(callback) {
+        request(baseUrl + 'stationInfo.aspx?stationId=' + toStationId, function(error, response, html) {
+          var $$ = cheerio.load(html);
+
+          var scraped = $$('.displayDriverData').find('tr td');
+          var stationData = {
+            location: {
+              street: scraped.eq(0).find('span').eq(0).text(),
+              postalCode: scraped.eq(0).find('span').eq(1).text(),
+              city: scraped.eq(0).find('span').eq(2).text()
+            },
+            contact: {
+              email: scraped.eq(1).find('span').eq(1).text(),
+              phone: scraped.eq(1).find('span').eq(2).text()
+            }
+          };
+
+          callback(null, stationData);
+        });
+      }], function(err, result) {
+        parsed.push({
+          from: {
+            name: anchors.eq(0).text(),
+            id: fromStationId,
+            station: result[0]
           },
-          contact: {
-            email: scraped.eq(1).find('span').eq(1).text(),
-            phone: scraped.eq(1).find('span').eq(2).text()
-          }
-        };
-
-        callback(null, stationData);
-      })
-    }, function(callback) {
-      request(baseUrl + 'stationInfo.aspx?stationId=' + toStationId, function(error, response, html) {
-        var $$ = cheerio.load(html);
-
-        var scraped = $$('.displayDriverData').find('tr td');
-        var stationData = {
-          location: {
-            street: scraped.eq(0).find('span').eq(0).text(),
-            postalCode: scraped.eq(0).find('span').eq(1).text(),
-            city: scraped.eq(0).find('span').eq(2).text()
+          to: {
+            name: anchors.eq(1).text(),
+            id: toStationId,
+            station: result[1]
           },
-          contact: {
-            email: scraped.eq(1).find('span').eq(1).text(),
-            phone: scraped.eq(1).find('span').eq(2).text()
-          }
-        };
-
-        callback(null, stationData);
+          car: car,
+          startData: startDate,
+          endData: endData
+        });
+        theCallback();
       });
-    }], function(err, result) {
-      parsed.push({
-        from: {
-          name: anchors.eq(0).text(),
-          id: fromStationId,
-          station: result[0]
-        },
-        to: {
-          name: anchors.eq(1).text(),
-          id: toStationId,
-          station: result[1]
-        },
-        car: car,
-        startData: startDate,
-        endData: endData
-      });
-      theCallback();
+    }, function() {
+      res.json(parsed);
     });
-  }, function() {
-    console.log(JSON.stringify(parsed[0]));
   });
 });
 
 
 
-
-
+app.listen(3001);
